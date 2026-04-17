ebabddaff1d5afc5d10100209a669355e9e6e90b
Okpunor Emmanuel <okpunorrex@gmail.com>
2026-04-18 00:47:05 +0300

    Add CORS middleware configuration to FastAPI application
    - Enhanced main.py by adding CORSMiddleware to enable cross-origin resource sharing.
    - Configured middleware to allow all origins, methods, and headers for improved API accessibility.

--- stage-2/app/main.py
+++ stage-2/app/main.py
@@ -5,8 +5,16 @@ from .util import CustomHTTPException
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi import Request
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
