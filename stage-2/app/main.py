1249a5c2938fb853a13e85e53dd5a504435b39ab
Okpunor Emmanuel <okpunorrex@gmail.com>
2026-04-18 00:42:55 +0300

    Add initial project structure and database setup for FastAPI application
    - Created necessary configuration files in the .idea directory for project management.
    - Added SQLite database setup with SQLModel for data persistence.
    - Implemented FastAPI application structure with routers and dependency injection for session management.
    - Defined models for profiles and included endpoints for CRUD operations on profiles.
    - Configured project metadata in pyproject.toml and specified Python version requirements.
    - Added .gitignore to exclude unnecessary files and directories.

---
+++ stage-2/app/main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from .routers import profiles
+from .database import create_db_and_tables
+from .util import CustomHTTPException
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+from fastapi import Request
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+app.include_router(profiles.router)
+
+
+@app.exception_handler(CustomHTTPException)
+async def custom_http_exception_handler(request: Request, exc: CustomHTTPException):
+    return JSONResponse(
+        status_code=exc.status_code, content={"status": "error", "message": exc.message}
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422, content={"status": "error", "message": "Invalid type"}
+    )
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, World!"}
